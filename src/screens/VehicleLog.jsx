575a6ab1666e11b497cdaafccf435d240116d586
PREM VISHWAKARMA <147607826+pvishwakarma09@users.noreply.github.com>
2025-04-25 09:24:51 +0530

    vehicle

---
+++ src/screens/VehicleLog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+
+function VehicleLog() {
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  )
+}
+
+export default VehicleLog
